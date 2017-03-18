e3c0f8129d6c9d39689f8e0528a411adb897af41
hmoneoju <h123FuWY>
2017-03-18 20:13:53 +0000

    Make Operation immutable

--- src/main/java/com/hmoneoju/evalme/model/Operation.java
+++ src/main/java/com/hmoneoju/evalme/model/Operation.java
@@ -8,20 +8,17 @@ public class Operation {
     private String expression;
     private String result;
 
+    public Operation(String expression, String result) {
+        this.expression = expression;
+        this.result = result;
+    }
+
     public String getExpression() {
         return expression;
     }
 
-    public void setExpression(String expression) {
-        this.expression = expression;
-    }
-
     public String getResult() {
         return result;
-    }
-
-    public void setResult(String result) {
-        this.result = result;
     }
 
 }
